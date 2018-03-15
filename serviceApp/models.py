40743bdcf1d3cfb8c37b94e51713cb3de1a630e0
Quanjiang Lee <QuanjiangLee@gmail.com>
2018-03-15 18:31:51 +0800

    nothing

--- serviceApp/models.py
+++ serviceApp/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class userInf(models.Model):
 	userNo = models.IntegerField(primary_key=True, null=False)
-	userPasswd = models.TextField()
+	userName = models.TextField()
 	def __str__(self):
-		return self.userPasswd
+		return self.userName
 
 class myServerMap(models.Model):
     #userNo = models.ForeignKey(userInf)
