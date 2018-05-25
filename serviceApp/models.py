798d519e7b7552a2744c41f22fcf9412d427c43a
Quanjiang <QuanjiangLee@gmail.com>
2018-05-25 17:03:19 +0800

    bug 修改

--- serviceApp/models.py
+++ serviceApp/models.py
@@ -18,7 +18,7 @@ class userInf(models.Model):
 
 class phonesInf(models.Model):
     phone_id = models.AutoField(primary_key=True, null=False)
-    admin_id = models.ForeignKey(userInf,related_name='phones_admin',on_delete=models.DO_NOTHING) 
+    admin_id = models.ForeignKey(userInf,related_name='phones_admin',on_delete=models.CASCADE) 
     phone_name = models.CharField(max_length = 30)
     image_path = models.CharField(max_length = 255)
     phone_details = models.TextField() 
@@ -27,9 +27,9 @@ class phonesInf(models.Model):
 
 class workOrders(models.Model):
     order_id = models.AutoField(primary_key=True, null=False)
-    user_id = models.ForeignKey(userInf,related_name='work_user', on_delete=models.DO_NOTHING)
-    server_id = models.ForeignKey(userInf,related_name='work_server',on_delete=models.DO_NOTHING)
-    phone_id = models.ForeignKey(phonesInf, on_delete=models.DO_NOTHING)
+    user_id = models.ForeignKey(userInf,related_name='work_user', on_delete=models.CASCADE)
+    server_id = models.ForeignKey(userInf,related_name='work_server', on_delete=models.CASCADE)
+    phone_id = models.ForeignKey(phonesInf,related_name='work_phone', on_delete=models.CASCADE)
     order_title = models.CharField(max_length = 30)
     order_details = models.CharField(max_length = 255)
     order_status = models.BooleanField(default=False)
@@ -42,9 +42,9 @@ class workOrders(models.Model):
     	return self.order_title
 
 class commitDetails(models.Model):
-    commit_id = models.ForeignKey(workOrders,related_name='commit_order', on_delete=models.DO_NOTHING)
-    commit_from = models.ForeignKey(userInf, related_name='commit_from_user', on_delete=models.DO_NOTHING)
-    commit_to = models.ForeignKey(userInf,related_name='commit_to_user', on_delete=models.DO_NOTHING)
+    commit_id = models.ForeignKey(workOrders,related_name='commit_order', on_delete=models.CASCADE)
+    commit_from = models.ForeignKey(userInf, related_name='commit_from_user', on_delete=models.CASCADE)
+    commit_to = models.ForeignKey(userInf,related_name='commit_to_user', on_delete=models.CASCADE)
     commit_details = models.CharField(max_length = 255)
     commit_status = models.BooleanField(default=False)
     commit_time = models.DateTimeField(auto_now_add = True)
@@ -56,9 +56,9 @@ class commitDetails(models.Model):
 
 
 class gradesInf(models.Model):
-    grade_id = models.ForeignKey(workOrders,related_name='grade_order', on_delete=models.DO_NOTHING)
-    user_id = models.ForeignKey(userInf,related_name='grade_user', on_delete=models.DO_NOTHING)
-    server_id = models.ForeignKey(userInf, on_delete=models.DO_NOTHING)
+    grade_id = models.ForeignKey(workOrders,related_name='grade_order', on_delete=models.CASCADE)
+    user_id = models.ForeignKey(userInf,related_name='grade_user', on_delete=models.CASCADE)
+    server_id = models.ForeignKey(userInf, on_delete=models.CASCADE)
     user_grade = models.FloatField()
     user_message =  models.TextField()
     grade_time = models.DateTimeField(auto_now_add = True)
@@ -66,7 +66,7 @@ class gradesInf(models.Model):
         return self.user_message
 
 class aboutInf(models.Model):
-    admin_id = models.ForeignKey(userInf,related_name='about_admin',  on_delete=models.DO_NOTHING)
+    admin_id = models.ForeignKey(userInf,related_name='about_admin',  on_delete=models.CASCADE)
     about_title = models.CharField(max_length = 30)
     image_path = models.CharField(max_length = 255)
     about_content = models.TextField()
@@ -77,7 +77,7 @@ class aboutInf(models.Model):
     
 class carParts(models.Model):
     partId = models.AutoField(primary_key=True, null=False)
-    personId = models.ForeignKey(userInf,related_name='admin_user',  on_delete=models.DO_NOTHING)
+    personId = models.ForeignKey(userInf,related_name='admin_user',  on_delete=models.CASCADE)
     partName = models.CharField(max_length = 40)
     partNumber = models.IntegerField()
     partType = models.CharField(max_length = 10)
@@ -94,13 +94,13 @@ class repairInfo(models.Model):
     repairDetails = models.CharField(max_length = 255)
     partDetails = models.CharField(max_length = 255)
     repairDate = models.DateTimeField(auto_now_add = True)
-    personId = models.ForeignKey(userInf,related_name='repair_user',  on_delete=models.DO_NOTHING)
+    personId = models.ForeignKey(userInf,related_name='repair_user',  on_delete=models.CASCADE)
     def __str__(self):
         return self.repairName
 '''
 class repairDetails(models.Model):
-    detailsId = models.ForeignKey(repairInfo,related_name='repair_id',  on_delete=models.DO_NOTHING)
-    detailPartId = models.ForeignKey(carParts,related_name='part_id',  on_delete=models.DO_NOTHING)
+    detailsId = models.ForeignKey(repairInfo,related_name='repair_id',  on_delete=models.CASCADE)
+    detailPartId = models.ForeignKey(carParts,related_name='part_id',  on_delete=models.CASCADE)
     partName = models.CharField(max_length = 40)
     partNum = models.IntegerField()
     sigleTotalPrice = models.FloatField()
